61fb5c7740b387991e950f93574a8838115b62d8
Nikita Trafilkin <ntrafilkin88@gmail.com>
2021-03-11 18:48:22 +0300

    adds basic middlewares

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send({ message: "Server is working" });
