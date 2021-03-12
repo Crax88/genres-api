bff5d3a8a6742a18720a7b6c73f9b95ef9b4720e
Nikita Trafilkin <ntrafilkin88@gmail.com>
2021-03-12 17:26:29 +0300

    little updates

--- routes/rentals.js
+++ routes/rentals.js
@@ -22,7 +22,7 @@ router
     const movie = await Movie.findById(req.body.movieId);
     if (!movie) return res.status(400).send("Movie not found");
     if (movie.numberInStock === 0) res.status(400).send("Movie not in stock");
-    let rental = new Rental({
+    const rental = new Rental({
       customer: {
         _id: customer._id,
         name: customer.name,
